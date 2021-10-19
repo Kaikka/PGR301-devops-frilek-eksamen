0c5eb8f49fefa91d2b5caceaabd769ededc1bab2
Kaikka <kaileeto@gmail.com>
2021-10-19 09:49:52 +0200

    idiot test

--- src/test/java/no/kristiania/frilekeksamen/FrilekEksamenApplicationTests.java
+++ src/test/java/no/kristiania/frilekeksamen/FrilekEksamenApplicationTests.java
@@ -3,11 +3,15 @@ package no.kristiania.frilekeksamen;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class FrilekEksamenApplicationTests {
 
     @Test
-    void contextLoads() {
+    void idiotTest() {
+        int x = 20;
+        assertEquals(x, 20);
     }
 
 }
